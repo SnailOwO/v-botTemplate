b35487cb0d2d000a1ca4ff1ebe142a6bdf7bcda4
SnailOwO <892333187@qq.com>
2018-07-05 17:45:01 +0800

    环境初步搭建中

--- src/main.js
+++ src/main.js
@@ -2,14 +2,26 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
+import iView from 'iview'
+import Routers from './router.js'
+import VueRouter from 'vue-router'
+import 'iview/dist/styles/iview.css'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(iView)
+
+// Vue.config.productionTip = false
+
+const RouterConfig = {
+  routes: Routers
+}
+const router = new VueRouter(RouterConfig)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
