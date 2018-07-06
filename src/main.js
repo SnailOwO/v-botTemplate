cb67f2cb90dce5712b62c49f1a3c4a84ed0c9194
SnailOwO <892333187@qq.com>
2018-07-06 17:41:17 +0800

    axios的初步配置

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router/index'
 import iView from 'iview'
+import axios from './axios'
 import 'iview/dist/styles/iview.css'
 import 'src/css/common.less'
 
 Vue.use(iView)
+Vue.use(axios)
 /* eslint-disable no-new */
 new Vue({
   router,
