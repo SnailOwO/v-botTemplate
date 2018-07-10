c1a52875072d250c676fb3d321983a2a9b26aec7
SnailOwO <892333187@qq.com>
2018-07-10 17:03:34 +0800

    common.less中其他css文件引入的问题

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,12 +1,9 @@
 <template>
-  <div class="hello">
-    12222
-  </div>  
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
