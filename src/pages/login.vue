5ba63219ffaa1d7edae22965064a3de7cdc45282
SnailOwO <892333187@qq.com>
2018-07-12 17:29:01 +0800

    login 页面 走马灯样式提交

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -7,25 +7,27 @@
     </ul>
     <!-- header end -->
 
-    <div class="login-carousel">
-      <Carousel autoplay v-model="first_index" loop>
-        <CarouselItem>
-            <div class="demo-carousel">1</div>
-        </CarouselItem>
-        <CarouselItem>
-            <div class="demo-carousel">2</div>
-        </CarouselItem>
-        <CarouselItem>
-            <div class="demo-carousel">3</div>
-        </CarouselItem>
-        <CarouselItem>
-            <div class="demo-carousel">4</div>
-        </CarouselItem>
-      </Carousel>
-    </div>
-
     <!-- login start -->
     <div class="login-content">
+      <!-- carousel start -->
+      <div class="login-carousel">
+        <Carousel autoplay v-model="first_index" loop class="custom-carousel">
+          <CarouselItem>
+              <div class="carousel-item"><img src="/static/images/funny_rain/13.png"></div>
+          </CarouselItem>
+          <CarouselItem>
+              <div class="carousel-item">2</div>
+          </CarouselItem>
+          <CarouselItem>
+              <div class="carousel-item">3</div>
+          </CarouselItem>
+          <CarouselItem>
+              <div class="carousel-item">4</div>
+          </CarouselItem>
+        </Carousel>
+      </div>
+      <!-- carousel end -->
+
       <!-- name start -->
       <div class="name-box">
         <i-input :placeholder="this.$t('login.page.username')" class="login-input"></i-input>
