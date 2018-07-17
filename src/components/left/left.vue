53a5ac91616b4e8ea46d9e105e44a01b6a913d8a
SnailOwO <892333187@qq.com>
2018-07-17 17:33:12 +0800

    left sider 调整初步提交

--- src/components/left/left.vue
+++ src/components/left/left.vue
@@ -1,20 +1,18 @@
 <template>
-    <Sider collapsible :collapsed-width="78" v-model="collapsed">
-        <Menu active-name="1-2" theme="dark" width="auto" :class="menuitemClasses">
-            <MenuItem name="1-1">
-                <Icon type="ios-navigate"></Icon>
-                <span>Option 1</span>
-            </MenuItem>
-            <MenuItem name="1-2">
-                <Icon type="search"></Icon>
-                <span>Option 2</span>
-            </MenuItem>
-            <MenuItem name="1-3">
-                <Icon type="settings"></Icon>
-                <span>Option 3</span>
-            </MenuItem>
-        </Menu>
-    </Sider>
+    <Menu active-name="1-2" theme="dark" width="auto" :class="menuitemClasses">
+        <MenuItem name="1-1">
+            <Icon type="ios-navigate"></Icon>
+            <span>Option 1</span>
+        </MenuItem>
+        <MenuItem name="1-2">
+            <Icon type="search"></Icon>
+            <span>Option 2</span>
+        </MenuItem>
+        <MenuItem name="1-3">
+            <Icon type="settings"></Icon>
+            <span>Option 3</span>
+        </MenuItem>
+    </Menu>
 </template>
 
 <script>
@@ -35,7 +33,7 @@ export default {
     }
   },
   computed: {
-    
+
   },
   props: ['collapsed']
 }
--- src/pages/mainLayout.vue
+++ src/pages/mainLayout.vue
@@ -2,7 +2,9 @@
   <div class="layout">
     <headerNav></headerNav>
     <Layout :style="{minHeight: '100vh'}">
-        <leftNav :collapsed="isCollapsed"></leftNav>
+        <Sider collapsible :collapsed-width="78" v-model="isCollapsed">
+          <leftNav :collapsed="isCollapsed"></leftNav>
+        </Sider>
         <Layout>
             <!-- <Header :style="{background: '#fff', boxShadow: '0 2px 3px 2px rgba(0,0,0,.1)'}"></Header> -->
             <contentNav></contentNav>
