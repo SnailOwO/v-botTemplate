c0985f6992dc236ee737701769dd3fc057d9c41e
SnailOwO <892333187@qq.com>
2018-07-18 17:48:50 +0800

    header 头部文件增加+操作按钮

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -4,6 +4,9 @@
       <div class="logo"></div>
       <label class="website-name">V-bot</label>
     </div>
+    <div class="extra-operate">
+      <i class="icon-add-vm icon-size ivu-icon ivu-icon-plus-circled"></i>
+    </div>
     <div class="user-box">
       <div class="user-head">
         <img :src="headImg" :alt="this.$t('common.header.headImage')" />
@@ -11,11 +14,11 @@
       <div class="user-name">
         <label>admin</label>
         <i class="dropdown ivu-icon ivu-icon-arrow-down-b"></i>
+        <ul class="user-menu">
+          <li>修改密码</li>
+          <li>退出登录</li>
+        </ul>
       </div>
-      <ul class="user-menu">
-        <li>修改密码</li>
-        <li>退出登录</li>
-      </ul>
     </div>
   </div>  
 </template>
@@ -27,11 +30,21 @@ export default {
       //随机头像。头像从登录页的滑稽rain中抽取
       extenstion: '.png',  
       baseEmojiPath: '/static/images/funny_rain/',
-      headImg: ''
+      headImg: '',
+      time: ''
     }
   },
   mounted() {
     this.headImg = this.randomEmoji(1,13);
+    if(this.time) {
+      clearInterval(this.time);
+    }
+    this.time = setInterval(() => {
+      this.headImg = this.randomEmoji(1,13);
+    }, 10000);
+  },
+  beforeDestroy: function () {
+    clearInterval(this.time);
   },
   methods: {
     randomEmoji(min,max) {
